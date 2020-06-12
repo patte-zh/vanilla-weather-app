11eaeedb67990912ddc0ffcdaeaf1b7aef0460ec
patte-zh <64026251+patte-zh@users.noreply.github.com>
2020-06-12 17:30:09 +0100

    icon updated

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ function showTemp(response) {
   let iconElement = document.querySelector(`#icon`);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.date.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
